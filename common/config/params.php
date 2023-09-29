8b11cd288541414d6a73b4e6f21645c5ac72c2a0
Rizki Agung Sentosa <77620549+rizkiagungid@users.noreply.github.com>
2023-09-29 19:05:53 +0700

    OK FIX
    OK FIX

---
+++ common/config/params.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'adminEmail' => 'admin@example.com',
+    'supportEmail' => 'support@example.com',
+    'user.passwordResetTokenExpire' => 3600,
+    'flashMessagePositionY' =>'top',
+    'flashMessagePositionX' =>'center',
+    'footerInfoLeft'=>'Hak Cipta ',
+    'footerInfoRight'=>'INLISLite v3.2',
+    'urlActivation' => 'http://demo.inlislitev3.perpusnas.go.id/api/web/v1',
+    'year' => '2017 - 2018'
+    // 'year' => date('Y') tahun mengikuti tanggalan PC
+
+];
---
+++ frontend/views/site/index.php
@@ -0,0 +1,71 @@
+<?php
+
+/* @var $this yii\web\View */
+use yii\helpers\Url;
+
+$this->title = 'Portal Aplikasi Perpustakaan & Galeri Kota Bogor';
+?>
+<link rel="shortcut icon" type='image/x-icon'
+    href="<?= Yii::$app->urlManager->createUrl('../uploaded_files/aplikasi/logo-perpuskotabogor.jpg'); ?>">
+
+<div class="container">
+    <div class="row">
+
+        <center>
+            <?= yii\helpers\Html::img(Url::base() . '/' . Url::to('uploaded_files/aplikasi/logo-perpuskotabogor2.jpg'), ['alt' => 'Portal Aplikasi Perpustakaan & Galeri Kota Bogor', 'class' => 'thing', 'style' => 'width:38%; margin-bottom:34px;']) ?>
+
+            <p>
+                <a class="btn btn-primary btn3d" target="_blank" href="<?= Url::to('backend') ?>"> Back Office</a>
+                <!-- <a class="btn btn-primary btn3d" target="_blank" href="<?= Url::to('bacaditempat') ?>">
+                    <?= Yii::t('app', 'Baca ditempat') ?>
+                </a> -->
+                <a class="btn btn-primary btn3d" target="_blank" href="<?= Url::to('guestbook') ?>">
+                    <?= Yii::t('app', 'Buku Tamu') ?>
+                </a>
+                <!-- <a class="btn btn-primary btn3d" target="_blank" href="<?= Url::to('keanggotaan') ?>">
+                    <?= Yii::t('app', 'Keanggotaan Online') ?>
+                </a>
+                <a class="btn btn-primary btn3d" target="_blank" href="<?= Url::to('digitalcollection') ?>">
+                    <?= Yii::t('app', 'Layanan Koleksi Digital') ?>
+                </a>
+                <a class="btn btn-primary btn3d" target="_blank" href="<?= Url::to('opac') ?>">
+                    <?= Yii::t('app', 'OPAC') ?>
+                </a>
+                <a class="btn btn-primary btn3d" target="_blank" href="<?= Url::to('article') ?>">
+                    <?= Yii::t('app', 'Artikel') ?>
+                </a>
+                <a class="btn btn-primary btn3d" target="_blank" href="<?= Url::to('pendaftaran') ?>">
+                    <?= Yii::t('app', 'Pendaftaran Anggota') ?>
+                </a> -->
+                <!-- <a class="btn btn-primary btn3d" target="_blank"
+                    href="<?= Url::to('statistik-perkembangan-perpustakaan') ?>">
+                    <?= Yii::t('app', 'Statistik') ?>
+                </a>
+                <a class="btn btn-primary btn3d" target="_blank" href="<?= Url::to('survey') ?>">
+                    <?= Yii::t('app', 'Survey') ?>
+                </a> -->
+
+                <!-- <a class="btn btn-primary btn3d" target="_blank" href="<?= Url::to('pengembalianmandiri') ?>">
+                    <?= Yii::t('app', 'Pengembalian Mandiri') ?>
+                </a>
+                <a class="btn btn-primary btn3d" target="_blank" href="<?= Url::to('peminjamanmandiri') ?>">
+                    <?= Yii::t('app', 'Peminjaman Mandiri') ?> -->
+                </a>
+            </p>
+
+        </center>
+    </div>
+</div>
+
+<div class="site-index">
+    <center>
+        <div class="mediumtron">
+            <p>
+
+            </p>
+
+        </div>
+    </center>
+    <div class="body-content">
+    </div>
+</div>
